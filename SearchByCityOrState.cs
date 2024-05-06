af4e946d0b7cc061fb376f62e74da67f838d7e5b
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-06 10:27:06 +0530

    Searching person by City or State across multiple Address  Book

---
+++ SearchByCityOrState.cs
@@ -0,0 +1,39 @@
+﻿
+using AddressBook;
+namespace AddressBook
+{
+    public class SearchByCityOrState
+    {
+        public List<Contacts> SearchContactsByCity(Dictionary<string, Address_Book> addressBooks, string city)
+        {
+            List<Contacts> results = new List<Contacts>();
+            foreach (var bookEntry in addressBooks.Values)
+            {
+                foreach (var contact in bookEntry.contacts)
+                {
+                    if (contact.City.Equals(city, StringComparison.OrdinalIgnoreCase))
+                    {
+                        results.Add(contact);
+                    }
+                }
+            }
+            return results;
+        }
+
+        public List<Contacts> SearchContactsByState(Dictionary<string, Address_Book> addressBooks, string state)
+        {
+            List<Contacts> results = new List<Contacts>();
+            foreach (var bookEntry in addressBooks.Values)
+            {
+                foreach (var contact in bookEntry.contacts)
+                {
+                    if (contact.State.Equals(state, StringComparison.OrdinalIgnoreCase))
+                    {
+                        results.Add(contact);
+                    }
+                }
+            }
+            return results;
+        }
+    }
+}
