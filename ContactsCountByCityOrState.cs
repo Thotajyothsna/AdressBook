17e068c14c58b7215bfc25304936b8c896fa72b4
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-06 10:31:35 +0530

    contacts count by City or State

---
+++ ContactsCountByCityOrState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class ContactsCountByCityOrState
+    {
+        public int CountContactsByCity(string city, CityStateAddressBook cityStateAddressBook)
+        {
+            if (cityStateAddressBook.cityContacts.ContainsKey(city))
+            {
+                return cityStateAddressBook.cityContacts[city].Count;
+            }
+            else
+            {
+                Console.WriteLine($"No contacts found for city '{city}'.");
+                return 0;
+            }
+        }
+
+        public int CountContactsByState(string state, CityStateAddressBook cityStateAddressBook)
+        {
+            if (cityStateAddressBook.stateContacts.ContainsKey(state))
+            {
+                return cityStateAddressBook.stateContacts[state].Count;
+            }
+            else
+            {
+                Console.WriteLine($"No contacts found for state '{state}'.");
+                return 0;
+            }
+        }
+    }
+
+}
